e8192589ac76104603bd8434bba39ee9e23eed2a
rahulmane121212 <107107447+rahulmane121212@users.noreply.github.com>
2023-01-27 22:07:36 +0530

    Add files via upload

--- employee/src/main/java/com/employee/employee/controller/EmpController.java
+++ employee/src/main/java/com/employee/employee/controller/EmpController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.employee.employee.exception.ResourceNotFoundException;
 import com.employee.employee.model.Employee;
 import com.employee.employee.service.EmpService;
 
@@ -31,5 +34,20 @@ public class EmpController {
 	public ResponseEntity< List < Employee >> getEmps(){
 		return ResponseEntity.ok().body(empService.getEmps());
 		
+		}
+	
+	@GetMapping("/getEmpById/{empId}")
+	public ResponseEntity < Employee > getEmpById(@PathVariable String empId) throws ResourceNotFoundException{
+		return ResponseEntity.ok().body(empService.getEmpById(empId));
+		
 	}
+	
+	@PutMapping("/updateEmpById/{empId}")
+	public ResponseEntity < Employee > updateEmp(@PathVariable String empId, @RequestBody Employee employee) throws ResourceNotFoundException {
+		employee.setEmpId(empId);
+		return ResponseEntity.ok().body(this.empService.updateEmp(employee));
+		
+	}
+	
+	
 }
